10c59e107088e61de505c9dbc3debf6c12f341c9
evanhanawalt <hanawalt.8@osu.edu>
2018-03-05 17:28:10 -0500

    python class updated

--- GameBoard.py
+++ GameBoard.py
@@ -1,15 +1,18 @@
 class GameBoard:
+
 	def __init__(self, rows = 6, columns = 8, prevboard = None):
 		self.board = []
 		self.rows = rows
 		self.columns columns
 
+		# initialize blank board
 		for i in range(0, rows):
 			self.board.append([])
 			for j in range(0, columns):
 				self.board[i].append(False)
 
-		if prevboard is None:
+		# fill from previous board if included in the constructor call
+		if prevboard is not None:
 			for i in range(0,rows):
 				if i >= prevboard.rows:
 					continue
@@ -20,8 +23,60 @@ class GameBoard:
 						self.set(i,j,prevboard.get(i,j));
 
 
-	def get(row, column):
+	def get(self, row, column):
 		return self.board[row][column]
 
-	def set(row, column, value):
+	def set(self, row, column, value):
 		self.board[row][column] = value
+
+	def number_live_neighbors(self, row, column):
+		count = 0
+		neighbor_rows = [row, row + 1, row - 1]
+		neighbor_columns = [column, column + 1, column - 1]
+
+		neighbor_indexes = [(x,y) for x in neighbor_rows
+							for y in neighbor_columns
+							# assert neighbor is in bounds
+							if (0 <= x < self.rows)
+							and (0 <= y < self.columns)
+							 # assert neighbor is not the cell under test
+							and not (x == row and y == column) ]
+
+		# increment count for all live cells
+		for cell in neighbor_indexes:
+			if( self.get(cell[0],cell[1]) ):
+				count += 1
+
+		return count
+				
+
+	def update(self):
+		newBoard = []
+		
+		for i in range(0, rows):
+			new_board.append([])
+			for j in range(0, columns):
+				live_neighbors = self.number_live_neighbors(i,j)
+
+				if (live_neighbors < 2):
+					new_board.board[i].append(False)
+				elif (live_neighbors == 2):
+					new_board.board[i].append(self.get(i,j))
+				elif (live_neighbors == 3):
+					new_board.board[i].append(True)
+				elif (live_neighbors > 3):
+					new_board.board[i].append(False)
+
+		self.board = new_board
+
+
+
+
+
+
+
+
+
+
+
+
